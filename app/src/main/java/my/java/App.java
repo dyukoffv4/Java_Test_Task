3de948202781e874d758e9930489ea2fdd9c2ddc
dyukoffv4 <net998art@gmail.com>
2025-01-09 00:17:26 +0300

    psvm refactor

--- app/src/main/java/my/java/App.java
+++ app/src/main/java/my/java/App.java
@@ -4,30 +4,69 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Задание 1.");
-        Task1.run(5);
-        System.out.println();
-
-        System.out.println("Задание 2.");
-        Task2.run("Hello");
-        System.out.println();
-
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Задание 3. Введите градусы в цельсиях:");
-        int gradus = scanner.nextInt();
+        doTask1(null);
+        doTask2(null);
+        doTask3(scanner);
+        doTask4(scanner);
+
+        scanner.close();
+    }
+
+    public static void doTask1(Scanner scanner) {
+        System.out.println("Задание 1.");
+        int size = 5;
+        if (scanner != null) {
+            System.out.print("Введите размер массива: ");
+            size = scanner.nextInt();
+            scanner.nextLine();
+        }
+
+        Task1.run(size);
+        System.out.println();
+    }
+
+    public static void doTask2(Scanner scanner) {
+        System.out.println("Задание 2.");
+        String line = "Hello";
+        if (scanner != null) {
+            System.out.println("Введите строку:");
+            line = scanner.nextLine();
+        }
+        else System.out.println("Строка: " + line);
+
+        Task2.run(line);
+        System.out.println();
+    }
+
+    public static void doTask3(Scanner scanner) {
+        System.out.println("Задание 3.");
+        int gradus = 100;
+        if (scanner != null) {
+            System.out.print("Введите градусы в цельсиях: ");
+            gradus = scanner.nextInt();
+            scanner.nextLine();
+        }
+        else System.out.println("Градусы в Цельсиях:    " + gradus);
 
         BaseConverter converter = new BaseConverter();
         System.out.println("Градусы в Келивинах:   " + converter.convert(gradus));
         converter.setTargetScale(BaseConverter.TemperatureScale.FAHRENHEIT);
         System.out.println("Градусы в Фаренгейтах: " + converter.convert(gradus) + "\n");
+    }
 
-        System.out.println("Задание 4. Введите часы и минуты:");
-        int hours = scanner.nextInt();
-        int minutes = scanner.nextInt();
+    public static void doTask4(Scanner scanner) {
+        System.out.println("Задание 4.");
+        int hours = 12, minutes = 15;
+        if (scanner != null) {
+            System.out.println("Введите часы и минуты через пробел:");
+            hours = scanner.nextInt();
+            minutes = scanner.nextInt();
+            scanner.nextLine();
+        }
+        else System.out.println("Время: " + hours + ":" + minutes);
 
-        System.out.println("Угол между часовой и минутной стрелками: " + Task4.calculateAngle(hours, minutes));
-
-        scanner.close();
+        System.out.println("Угол между часовой и минутной стрелками: " + Task4.calculateAngle(hours, minutes) + "\n");
     }
 }
