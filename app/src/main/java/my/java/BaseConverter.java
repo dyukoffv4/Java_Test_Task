1ca9cacb23886f680396d239b3f74ede516708d9
dyukoffv4 <net998art@gmail.com>
2025-01-09 00:17:46 +0300

    Comments add

--- app/src/main/java/my/java/BaseConverter.java
+++ app/src/main/java/my/java/BaseConverter.java
@@ -1,22 +1,43 @@
 package my.java;
 
+/**
+ * Класс BaseConverter предоставляет функциональность для перевода температуры
+ * из шкалы Цельсия в другие шкалы: Кельвина или Фаренгейта.
+ */
 public class BaseConverter {
     public enum TemperatureScale { KELVIN, FAHRENHEIT }
 
     private TemperatureScale targetScale;
 
+    /**
+     * Конструктор по умолчанию. Устанавливает целевую шкалу температуры в Кельвины.
+     */
     public BaseConverter() {
         this.targetScale = TemperatureScale.KELVIN;
     }
 
+    /**
+     * Конструктор с параметром. Позволяет указать целевую шкалу температуры.
+     * @param targetScale целевая шкала температуры (KELVIN или FAHRENHEIT)
+     */
     public BaseConverter(TemperatureScale targetScale) {
         this.targetScale = targetScale;
     }
 
+    /**
+     * Устанавливает целевую шкалу температуры.
+     * @param targetScale целевая шкала температуры (KELVIN или FAHRENHEIT)
+     */
     public void setTargetScale(TemperatureScale targetScale) {
         this.targetScale = targetScale;
     }
 
+    /**
+     * Конвертирует заданное значение температуры в градусах Цельсия в целевую шкалу.
+     * @param celsius температура в градусах Цельсия
+     * @return температура в целевой шкале (Кельвинах или Фаренгейтах)
+     * @throws IllegalStateException если указана неизвестная шкала температуры
+     */
     public double convert(double celsius) {
         switch (targetScale) {
             case KELVIN:
@@ -28,4 +49,3 @@ public class BaseConverter {
         }
     }
 }
-
--- app/src/main/java/my/java/Task4.java
+++ app/src/main/java/my/java/Task4.java
@@ -8,10 +8,19 @@ public class Task4 {
      * @return угол между стрелками в градусах
      */
     public static double calculateAngle(int hours, int minutes) {
+        // Приводим часы к значениям от 0 до 11
         hours = hours % 12;
+
+        // Вычисляем угол часовой стрелки относительно положения в 0 часов 0 минут.
         double hourAngle = (hours * 30) + (minutes / 2);
+
+        // Вычисляем угол минутной стрелки относительно положения в 0 часов 0 минут.
         double minuteAngle = minutes * 6;
+
+        // Выисляем угол между стрелками.
         double angle = Math.abs(hourAngle - minuteAngle);
+
+        // Определяем минимальный угол между стрелками.
         return Math.min(angle, 360 - angle);
     }
 }
